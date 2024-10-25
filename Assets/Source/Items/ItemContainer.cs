f3d9daa4d2d581debdf497fc79231fa1b380d3d7
Anton Chernykh <antoshidza@mail.ru>
2024-10-25 21:38:35 +0300

    Update ItemContainer.cs

--- Assets/Source/Items/ItemContainer.cs
+++ Assets/Source/Items/ItemContainer.cs
@@ -7,6 +7,7 @@ namespace Source.Items
     public class ItemContainer : MonoBehaviour
     {
         private readonly HashSet<Item> _containedItems = new();
+        private readonly HashSet<Item> _exitedItems = new();
         private int _itemCount;
 
         public event Action<int> ItemCountChanged; 
@@ -14,13 +15,14 @@ namespace Source.Items
         private void OnTriggerEnter(Collider other)
         {
             if (!other.transform.root.TryGetComponent<Item>(out var item) || !_containedItems.Add(item)) return;
+            _exitedItems.Remove(item);
             _itemCount++;
             ItemCountChanged?.Invoke(_itemCount);
         }
 
         private void OnTriggerExit(Collider other)
         {
-            if (!other.transform.root.TryGetComponent<Item>(out var item)) return;
+            if (!other.transform.root.TryGetComponent<Item>(out var item) || !_exitedItems.Add(item)) return;
             _containedItems.Remove(item);
             _itemCount--;
             ItemCountChanged?.Invoke(_itemCount);
